98503b4e69b953978194afdc8f7b4c631a057e94
Pranav Pokale <pranavpokale50034@gmail.com>
2025-02-21 14:32:28 +0530

    chore: / route

--- index.ts
+++ index.ts
@@ -14,13 +14,10 @@ interface Rooms {
 }
 
 const socketServer = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Google Meet Clone Server is Running! 🚀");
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
-  }
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({ message: "Google Meet Clone Server is Running! 🚀" })
+  );
 });
 
 const io = new SocketIOServer(socketServer, {
